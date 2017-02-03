bd26a72d48d16e534fb4dd8fb4c1acb1046696c1
ashraf-s9s <ashraf@severalnines.com>
2017-02-03 22:49:01 +0800

    CC nightly build with docker autoprovisioning

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ClusterControl Docker Image #
+# ClusterControl Docker Image - Development (nightly) version#
 
 ## Table of Contents ##
 
@@ -33,7 +33,7 @@ More details at [Severalnines](http://www.severalnines.com/clustercontrol) websi
 
 To pull ClusterControl images, simply:
 ```bash
-$ docker pull severalnines/clustercontrol
+$ docker pull severalnines/clustercontrol:nightly
 ```
 
 The image consists of ClusterControl and all of its components:
@@ -46,17 +46,17 @@ The image consists of ClusterControl and all of its components:
 
 To run a ClusterControl container, the simplest command would be:
 ```bash
-$ docker run -d severalnines/clustercontrol
+$ docker run -d severalnines/clustercontrol:nightly
 ```
 
 However, we would recommend users to assign a container name and map the host's port with exposed HTTP or HTTPS port on container:
 ```bash
-$ docker run -d --name clustercontrol -p 5000:80 severalnines/clustercontrol
+$ docker run -d --name clustercontrol -p 5000:80 severalnines/clustercontrol:nightly
 ```
 
 Verify with:
 ```bash
-$ docker logs clustercontrol
+$ docker logs -f clustercontrol
 $ docker ps # ensure the container is started and running
 ```
 
@@ -96,9 +96,9 @@ $ docker run -d --name clustercontrol -p 5000:80 -p 5443:443 severalnines/cluste
 
 To build Docker image, download the Docker related files available at [our Github repository](https://github.com/severalnines/docker):
 ```bash
-$ git clone https://github.com/severalnines/docker
-$ cd docker
-$ docker build -t severalnines/clustercontrol .
+$ git clone https://github.com/ashraf-s9s/clustercontrol-docker
+$ cd clustercontrol-docker
+$ docker build -t severalnines/clustercontrol:nightly .
 ```
 
 Verify with:
@@ -106,26 +106,34 @@ Verify with:
 $ docker images
 ```
 
-## Adding an Existing Cluster ##
+## How to Use ##
 
-1) Ensure your database cluster is up and running. Supported database cluster is listed under [Overview](#overview) section.
-
-2) Copy the auto-generated SSH key on ClusterControl to the target database containers/nodes. For example, if your database containers' IP address is 172.17.0.11,172.17.0.12,172.17.0.13 run following command on ClusterControl node:
+1) Run the ClusterControl container:
 ```bash
-[host]$ docker exec -it [clustercontrol_container_name] /bin/bash
-[container]$ ssh-copy-id 172.17.0.11
-[container]$ ssh-copy-id 172.17.0.12
-[container]$ ssh-copy-id 172.17.0.13
+docker run -d --name clustercontrol -p 5000:80 severalnines/clustercontrol:nightly
 ```
 
-3) Access the ClusterControl UI and click on *Add Existing Server/Cluster* button. Enter required details and click *Add Cluster*. 
+2) Run the DB containers (replace `CC_HOST` value accordingly):
+```bash
+docker run -d --name galera1 -p 6661:3306 -e CC_HOST=172.17.0.2 -e CLUSTER_TYPE=galera -e CLUSTER_NAME=mygalera -e INITIAL_CLUSTER_SIZE=3 severalnines/centos-ssh
+docker run -d --name galera2 -p 6662:3306 -e CC_HOST=172.17.0.2 -e CLUSTER_TYPE=galera -e CLUSTER_NAME=mygalera -e INITIAL_CLUSTER_SIZE=3 severalnines/centos-ssh
+docker run -d --name galera3 -p 6663:3306 -e CC_HOST=172.17.0.2 -e CLUSTER_TYPE=galera -e CLUSTER_NAME=mygalera -e INITIAL_CLUSTER_SIZE=3 severalnines/centos-ssh
+```
+
+3) ClusterControl will automatically pick the new containers to deploy. If it finds the number of containers is equal or greater than `INITIAL_CLUSTER_SIZE`, the cluster deployment shall begin. You can verify that with:
+```bash
+docker logs -f clustercontrol
+```
+
+Or, open ClusterControl UI and look under Activity (top menu).
 
 
-## Limitations ##
-
-* The image are tested and built using Docker version 1.5.0-dev, build fc0329b and Docker version 1.6.0, build bdbc177 on CentOS 7.1.
-
-* [ClusterControl known issues and limitations](http://severalnines.com/docs/troubleshooting.html#known-issues-and-limitations).
+4) To scale up, just create another nodes and ClusterControl will add into the cluster automatically:
+```bash
+docker run -d --name galera4 -p 6664:3306 -e CC_HOST=172.17.0.2 -e CLUSTER_TYPE=galera -e CLUSTER_NAME=mygalera -e INITIAL_CLUSTER_SIZE=3 severalnines/centos-ssh
+docker run -d --name galera5 -p 6665:3306 -e CC_HOST=172.17.0.2 -e CLUSTER_TYPE=galera -e CLUSTER_NAME=mygalera -e INITIAL_CLUSTER_SIZE=3 severalnines/centos-ssh
+```
+5) Repeat step #3.
 
 ## Development ##
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,9 @@ set -e
 
 CMON_CONFIG=/etc/cmon.cnf
 SSH_KEY=/root/.ssh/id_rsa
+MOUNT_SSH_KEY=/mnt/key/id_rsa
 WWWROOT=/var/www/html
+PUB_KEY_DIR=$WWWROOT/keys
 CMONAPI_BOOTSTRAP=$WWWROOT/cmonapi/config/bootstrap.php
 CMONAPI_DATABASE=$WWWROOT/cmonapi/config/database.php
 CCUI_BOOTSTRAP=$WWWROOT/clustercontrol/bootstrap.php
@@ -20,14 +22,16 @@ IP_ADDRESS=$(ip a | grep eth0 | grep inet | awk {'print $2'} | cut -d '/' -f 1 |
 DATADIR=/var/lib/mysql
 PIDFILE=${DATADIR}/mysqld.pid
 [ -f $PIDFILE ] && rm -f $PIDFILE
-echo 'Checking MySQL daemon..'
+echo
+echo '>> Checking MySQL daemon..'
 [ -z $(pidof mysqld_safe) ] && service mysql start || (killall -9 mysqld && service mysql start)
 
 # import data
 if [ ! -e $MYSQL_CMON_CNF ]; then
 	# configure ClusterControl Controller
 	CMON_TOKEN=$(python -c 'import uuid; print uuid.uuid4()' | sha1sum | cut -f1 -d' ')
-	echo "Setting up minimal $CMON_CONFIG.."
+	echo
+	echo ">> Setting up minimal $CMON_CONFIG.."
 	cat /dev/null > $CMON_CONFIG
 	cat > "$CMON_CONFIG" << EOF
 mysql_port=3306
@@ -36,8 +40,8 @@ mysql_password=$cmon_password
 hostname=$IP_ADDRESS
 rpc_key=$CMON_TOKEN
 EOF
-
-	echo 'Setting up ClusterControl UI and CMONAPI..'
+	echo
+	echo '>> Setting up ClusterControl UI and CMONAPI..'
 	## configure ClusterControl UI & CMONAPI
 	sed -i "s|GENERATED_CMON_TOKEN|$CMON_TOKEN|g" $CMONAPI_BOOTSTRAP
 	sed -i "s|^define('ENABLE_CC_API_TOKEN_CHECK'.*|define('ENABLE_CC_API_TOKEN_CHECK', '1');|g" $CMONAPI_BOOTSTRAP
@@ -51,21 +55,31 @@ EOF
 	## configure SSH
 	AUTHORIZED_FILE=/root/.ssh/authorized_keys
 	KNOWN_HOSTS=/root/.ssh/known_hosts
-	ssh-keygen -t rsa -N "" -f $SSH_KEY
+	if [ -f $MOUNT_SSH_KEY ]; then
+		cp $MOUNT_SSH_KEY $SSH_KEY
+		cp ${MOUNT_SSH_KEY}.pub ${SSH_KEY}.pub
+	else
+		ssh-keygen -t rsa -N "" -f $SSH_KEY
+	fi
 	cat ${SSH_KEY}.pub >> $AUTHORIZED_FILE
+	[ -d $PUB_KEY_DIR ] || mkdir -p $PUB_KEY_DIR
+	cat ${SSH_KEY}.pub >> $PUB_KEY_DIR/cc.pub
+	chown -Rf apache:apache  $PUB_KEY_DIR
 	KEY_TYPE=$(awk {'print $1'} ${SSH_KEY}.pub)
 	PUB_KEY=$(awk {'print $2'} ${SSH_KEY}.pub)
 	echo "$IP_ADDRESS $KEY_TYPE $PUB_KEY" >> $KNOWN_HOSTS
 	chmod 600 $AUTHORIZED_FILE
 	
-	echo 'Importing CMON data..'
+	echo
+	echo '>> Importing CMON data..'
 	mysql -uroot -h127.0.0.1 -e 'create schema cmon; create schema dcps;' && \
 		mysql -f -uroot -h127.0.0.1 cmon < /usr/share/cmon/cmon_db.sql && \
 			mysql -f -uroot -h127.0.0.1 cmon < /usr/share/cmon/cmon_data.sql && \
 				mysql -f -uroot -h127.0.0.1 dcps < $WWWROOT/clustercontrol/sql/dc-schema.sql
 
 	# configure CMON user & password
-	echo 'Configuring CMON user and MySQL root password..'
+	echo
+	echo '>> Configuring CMON user and MySQL root password..'
 	TMPFILE=/tmp/configure_cmon.sql
 	cat > "$TMPFILE" << EOF
 UPDATE mysql.user SET Password=PASSWORD('$mysql_root_password') WHERE User='root';
@@ -75,13 +89,16 @@ DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE DB='test' OR DB='test\\
 GRANT ALL PRIVILEGES ON *.* TO 'cmon'@'localhost' IDENTIFIED BY '$cmon_password' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'cmon'@'127.0.0.1' IDENTIFIED BY '$cmon_password' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'cmon'@'$IP_ADDRESS' IDENTIFIED BY '$cmon_password' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'cmon'@'%' IDENTIFIED BY '$cmon_password' WITH GRANT OPTION;
 REPLACE INTO dcps.apis(id, company_id, user_id, url, token) VALUES (1, 1, 1, 'https://127.0.0.1/cmonapi', '$CMON_TOKEN');
+CREATE TABLE cmon.containers (id INT PRIMARY KEY AUTO_INCREMENT, hostname VARCHAR(255), ip VARCHAR(128), cluster_type VARCHAR(128), cluster_name VARCHAR(255), platform VARCHAR(64), db_root_password VARCHAR(255), initial_size INT, deploying TINYINT NOT NULL DEFAULT 0, deployed TINYINT NOT NULL DEFAULT 0);
 FLUSH PRIVILEGES;
 EOF
 
 	mysql -uroot -h127.0.0.1 < $TMPFILE; rm -f $TMPFILE
-	
-	echo 'Configuring CMON MySQL defaults file..'
+
+	echo
+	echo '>> Configuring CMON MySQL defaults file..'
 	cat > "$MYSQL_CMON_CNF" << EOF
 [mysql_cmon]
 user=cmon
@@ -90,19 +107,44 @@ EOF
 
 fi
 
-## Start the services
+# Start the services
 service cmon restart
 service sshd restart
+sleep 5
+
+# Configure s9s CLI
+
+if [ ! -e $BANNER_FILE ]; then
+	echo
+	echo '>> Create user "dba" for s9s cli'
+	/usr/sbin/useradd dba
+	echo '>> Generating key for s9s cli'
+	/usr/bin/s9s user --create --generate-key --controller=”https://localhost:9501” --cmon-user=dba
+	S9S_CONF=/root/.s9s/s9s.conf
+	if [ -f $S9S_CONF ]; then
+		echo '>> Configuring s9s.conf'
+		echo 'controller_host_name = localhost' >> $S9S_CONF
+		echo 'controller_port      = 9501' >> $S9S_CONF
+		echo 'rpc_tls              = true' >> $S9S_CONF
+	fi
+
+	echo ">> Testing connection to ClusterControl via s9s CLI"
+	/usr/bin/s9s cluster --ping
+fi
+echo
+echo ">> Starting up the Docker deployment script"
+/deploy-container.sh &
 
 ## generate a README-IMPORTANT! file to notify the generated credentials
 if [ ! -e $BANNER_FILE ]; then
-	echo "Please remember following information which generated by the entrypoint file" > $BANNER_FILE
-	[ -z "$CMON_PASSWORD" ] && echo "Generated CMON password: $cmon_password" >> $BANNER_FILE || echo "CMON password: $cmon_password" >> $BANNER_FILE
+	echo "!! Please remember following information which generated during entrypoint !!" > $BANNER_FILE
+	[ -z "$CMON_PASSWORD" ] && echo ">> Generated CMON password: $cmon_password" >> $BANNER_FILE || echo "CMON password: $cmon_password" >> $BANNER_FILE
 	[ -z "$MYSQL_ROOT_PASSWORD" ] &&	echo "Generated MySQL root password: $mysql_root_password" >> $BANNER_FILE || echo "MySQL root password: $mysql_root_password" >> $BANNER_FILE
 	echo "Generated ClusterControl API Token: $CMON_TOKEN" >> $BANNER_FILE
 	echo "Detected IP address: $IP_ADDRESS" >> $BANNER_FILE
 	echo "To access ClusterControl UI, go to http://${IP_ADDRESS}/clustercontrol" >> $BANNER_FILE
-	echo "!! Remove this file once you notified !!" >> $BANNER_FILE
 fi
 
+echo ""
+echo ">> Starting HTTP in the foreground"
 /usr/sbin/httpd -D FOREGROUND
